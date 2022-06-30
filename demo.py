8cf23bca43b564ae1b0f54e6c2691bc7abd3cb63
Tsunghao Huang <tsunghao.huang@pads.rwth-aachen.de>
2022-06-30 10:52:50 +0200

    change demo to export .pnml file

--- demo.py
+++ demo.py
@@ -1,9 +1,13 @@
 import algorithm as synthesisRulesMiner
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
+from pm4py.objects.petri_net.exporter import exporter as pnml_exporter
 
 log = xes_importer.apply('./data/demo.xes')
 net, im, fm = synthesisRulesMiner.apply(log, order_type='bfs', theta=0.95, 
                                         c=0.9, use_new_rule=False)
-gviz = pn_visualizer.apply(net, im, fm)
-pn_visualizer.view(gviz)
+
+pnml_exporter.apply(net, im, "./demo.pnml", final_marking=fm)
+
+# gviz = pn_visualizer.apply(net, im, fm)
+# pn_visualizer.view(gviz)
